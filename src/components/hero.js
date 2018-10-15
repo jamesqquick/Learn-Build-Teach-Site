078e4ccc51959abba140304c43e979ac17bf07b2
James Quick <james.q.quick@outlook.com>
2018-10-14 20:32:25 -0500

    Added button to hero and broke hero item into it's own component

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../sass/hero.scss";
 import Button from "./button";
+import HeroItem from "./heroItem";
 
 function onStartLearningClick() {
   console.log("clicked");
@@ -10,22 +11,9 @@ export default () => {
   return (
     <div className="section">
       <div className="hero-items">
-        <div className="hero-item">
-          <h2 className="hero-item-title">Learn.</h2>
-          <hr className="title-underline" />
-          <p className="hero-item-subtitle">the latest and greatest</p>
-        </div>
-        <div className="hero-item">
-          <h2 className="hero-item-title">Build.</h2>
-          <hr className="title-underline" />
-          <p className="hero-item-subtitle">projects to reinforce learnings</p>
-        </div>
-        <div className="hero-item">
-          <h2 className="hero-item-title">Teach.</h2>
-          <hr className="title-underline" />
-
-          <p className="hero-item-subtitle">to help others grow and learn</p>
-        </div>
+        <HeroItem title="Learn." subtitle="the latest and greatest" />
+        <HeroItem title="Build." subtitle="projects to reinfornce learnings" />
+        <HeroItem title="Teach." subtitle="to help others grow and learn" />
       </div>
 
       <h4 className="hero-title">
@@ -33,7 +21,10 @@ export default () => {
         <span className="highlight">Design</span>, and{" "}
         <span className="highlight">Tools</span> together.
       </h4>
-      <Button btnText="Start Learning" onBtnClick={onStartLearningClick} />
+
+      <div className="text-center">
+        <Button btnText="Start Learning" onBtnClick={onStartLearningClick} />
+      </div>
     </div>
   );
 };
