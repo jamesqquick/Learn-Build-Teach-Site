5ec7779bcf1583363ac60ded71078a68278c6459
James Quick <james.q.quick@outlook.com>
2018-10-16 12:21:25 -0500

    Responsive design for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,29 +2,53 @@ import React from "react";
 import { Link } from "gatsby";
 import "../sass/navbar.scss";
 import logo from "../images/lbt-icon.svg";
+import hamburger from "../images/hamburger.svg";
 
-const Navbar = ({ siteTitle }) => (
-  <nav className="nav">
-    <div className="nav-content">
-      <div className="nav-brand">
-        <img src={logo} alt="" />
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navOpen: false
+    };
+  }
+  render = () => (
+    <nav className="nav">
+      <div className="nav-content">
+        <div className="flex flex-between flex-align-center full-width">
+          <div className="nav-brand">
+            <img src={logo} alt="Learn Build Teach Logo." />
+          </div>
+          <a id="hamburgerBtn">
+            <img
+              src={hamburger}
+              alt="Menu Button"
+              onClick={this.toggleNavbar}
+            />
+          </a>
+        </div>
+        <ul className={"nav-items" + (this.state.navOpen ? "" : " hidden-sm")}>
+          <li className="nav-item">
+            <Link to="/">Featured</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/">Courses</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/">Newsletter</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/">Contact</Link>
+          </li>
+        </ul>
       </div>
-      <ul className="nav-items">
-        <li className="nav-item">
-          <Link to="/">Featured</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/">Courses</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/">Newsletter</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/">Contact</Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+
+  toggleNavbar = () => {
+    this.setState({
+      navOpen: !this.state.navOpen
+    });
+  };
+}
 
 export default Navbar;
