785d302d348e5c391526e9bf4638ee58e3b2bd5b
James Quick <james.q.quick@outlook.com>
2018-10-16 21:54:36 -0500

    refactored course list item to be a generic card.  used position sticky for header

--- src/components/card.js
+++ src/components/card.js
@@ -3,6 +3,9 @@ import React from "react";
 export default function card(props) {
   return (
     <div className="card">
+      {props.overlayUrl && (
+        <a className="card-overlay" href={props.overlayUrl} />
+      )}
       <div className="card-content">
         <h3>{props.title}</h3>
         <p>{props.subtitle}</p>
--- src/components/courseList.js
+++ src/components/courseList.js
@@ -1,16 +1,14 @@
 import React from "react";
-import CourseListItem from "./courseListItem";
-import "../sass/courseList.scss";
+import Card from "./card";
+import "../sass/card.scss";
+import vsCodeCover from "../images/learn-vs-code-cover.svg";
+import coreFundamentalsCover from "../images/core-fundamentals-cover.svg";
+import chatAppCover from "../images/chat-app-cover.svg";
 
-import logo from "../images/lbt-icon.svg";
 export default function courseList() {
-  return (
-    <div>
-      <CourseListItem
-        title="Learn VS Code"
-        subtitle="Visual Studio Code is the most popular text editor for Web Development. Learning to harness its full power will drastically improve your efficiency and proficiency as a developer! Customization, extensions, and more!  Whether you’re new to Web Development or a seasoned vet, there’s still something to learn!  "
-        imgSrc={logo}
-      />
-    </div>
-  );
+  return <div className="card-list">
+      <Card title="Learn VS Code" subtitle="Visual Studio Code is the most popular text editor for Web Development. Learning to harness its full power will drastically improve your efficiency and proficiency as a developer! Customization, extensions, and more!  Whether you’re new to Web Development or a seasoned vet, there’s still something to learn!  " imgSrc={vsCodeCover} overlayUrl="https://www.learnvscode.com/" />
+      <Card title="Design and Build a Chat App" subtitle="Visual Studio Code is the most popular text editor for Web Development. Learning to harness its full power will drastically improve your efficiency and proficiency as a developer! Customization, extensions, and more!  Whether you’re new to Web Development or a seasoned vet, there’s still something to learn!  " imgSrc={chatAppCover} overlayUrl="https://www.learnvscode.com/" />
+      <Card title="Core Fundamentals of Web Development" subtitle="Visual Studio Code is the most popular text editor for Web Development. Learning to harness its full power will drastically improve your efficiency and proficiency as a developer! Customization, extensions, and more!  Whether you’re new to Web Development or a seasoned vet, there’s still something to learn!  " imgSrc={coreFundamentalsCover} overlayUrl="https://www.learnvscode.com/" />
+    </div>;
 }
