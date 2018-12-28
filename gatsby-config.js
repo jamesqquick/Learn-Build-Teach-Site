f8741615d09c6856769d4ef450d487512e1cd4c2
James Quick <james.q.quick@outlook.com>
2018-12-27 20:26:55 -0600

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,14 @@ module.exports = {
       }
     },
     "gatsby-plugin-offline",
-    "gatsby-plugin-sass"
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-130194764-1",
+        // Puts tracking script in the head instead of the body
+        head: true
+      }
+    }
   ]
 };
