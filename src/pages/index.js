05009100bf8f7eb7b03f1227ed114fbff43c0964
James Quick <james.q.quick@outlook.com>
2019-07-10 21:02:43 -0500

    Get rid of split content reference

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,15 @@
 import React from "react";
 import Hero from "../components/hero";
 import Layout from "../components/layout";
-import SplitContent from "../components/splitView";
 import { Section } from "../components/section";
 import CourseList from "../components/courseList";
 import YouTubeGallery from "../components/YouTubeGallery";
-import { FeaturedLeft, FeaturedRight } from "../components/Featured";
-
+import { StyledNewsletter } from "../elements/Newsletter"
 const IndexPage = () => (
   <Layout>
     <Section>
       <Hero />
     </Section>
-    <SplitContent>
-      <FeaturedLeft title="Featured Content"/>
-      <FeaturedRight 
-        title="Learn VS Code" 
-        description="Visual Studio Code is the most popular text editor for Web Development.  Learning to harness its full power will drastically improve your efficiency and proficiency as a developer!"
-        linkUrl="https://www.udemy.com/learn-visual-studio-code/?couponCode=LEARNVSCODE10"
-      />
-    </SplitContent>
     <div id="courses">
       <Section title="Start Learning!">
         <CourseList />
@@ -28,6 +18,11 @@ const IndexPage = () => (
     <div id="recent">
       <Section title="Recent Videos" classString="section-dark">
         <YouTubeGallery />
+      </Section>
+    </div>
+    <div id="newsletter">
+      <Section  classString="section-dark">
+        <StyledNewsletter />
       </Section>
     </div>
   </Layout>
