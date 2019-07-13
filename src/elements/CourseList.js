14cf7340aa1cc7c9beb7c0051f70dcebd5c49298
James Quick <james.q.quick@outlook.com>
2019-07-13 18:17:16 -0500

    Updated feature card to be horizontal and added consistent graphics for courses

---
+++ src/elements/CourseList.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import {primary} from "../utilities/Colors";
+
+export const StyledCourseList = styled.div`
+    h1 {
+        color: ${primary};
+        margin-bottom: 2rem;
+    }
+
+    .callout {
+        text-align: center;
+    }
+`;
