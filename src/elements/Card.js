26e8de86b3e9114678d5e829cb818a05ea953760
James Quick <james.q.quick@outlook.com>
2019-07-14 13:17:42 -0500

    Updated card image to block instead of inline block

--- src/elements/Card.js
+++ src/elements/Card.js
@@ -77,6 +77,7 @@ export const CardImage = styled.div`
            width: 100%;
            height: 100%;
            object-fit: cover;
+           display: block
          }
 
          @media only screen and (max-width: 1000px) {order: -1;
