e3ff9f5e212cd3576edec76f4f5ecb3609e80b7e
James Quick <james.q.quick@outlook.com>
2019-07-14 11:41:59 -0500

    Mobile design stuff

--- src/elements/Forms.js
+++ src/elements/Forms.js
@@ -2,23 +2,32 @@ import styled from "styled-components";
 import {lightGrey} from "../utilities/Colors";
 
 export const StyledForm = styled.form`
+         max-width: 800px;
+         margin: 0 auto;
+         input {
+           padding: 1rem 1rem;
+           font-size: 1.4rem;
+           width: 100%;
+           border-radius: 5px;
+           border: 1px solid ${lightGrey};
+         }
 
-    max-width: 800px;
-    margin: 0 auto;
-    input {
-        padding: 1rem 1rem;
-        font-size: 1.4rem;
-        width: 100%;
-        border-radius: 5px;
-        border: 1px solid ${lightGrey};
-    }
+         .inline {
+           input {
+             margin-right: 1rem;
+           }
+           display: flex;
+         }
 
-    .inline {
-        input {
-            margin-right: 1rem;
-        }
-        display:flex;
-    }
-
-
-`;
+         @media only screen and (max-width: 768px) {
+            .inline {
+                input {
+                    margin-right: 0;
+                    margin-bottom: 1rem;
+                }
+                button {
+                    width:100%;
+                }
+                display:block;
+            }
+        }`;
--- src/elements/Newsletter.js
+++ src/elements/Newsletter.js
@@ -3,37 +3,23 @@ import styled from "styled-components";
 
 export const StyledNewsletter = styled.div`
 
-    .newsletter {
-  max-width: 500px;
-  margin: 0 auto;
+  .newsletter {
+    max-width: 500px;
+    margin: 0 auto;
 
-  .title {
-    font-size: 2.4rem;
-    font-weight: 300;
-    margin-bottom: 2rem;
-    text-align: center;
+    .title {
+      font-size: 2.4rem;
+      font-weight: 300;
+      margin-bottom: 2rem;
+      text-align: center;
+    }
   }
-}
 
-.inline-form-input {
-  display: flex;
-}
-
-.inline-form-input > *:first-child {
-  flex-grow: 1;
-}
 
 @media only screen and (max-width: 768px) {
   .newsletter {
     max-width: 400px;
   }
-  .inline-form-input {
-    flex-direction: column;
-  }
-
-  .inline-form-input > *:first-child {
-    flex-grow: 1;
-    margin-bottom: 1rem;
-  }
+ 
 }
 `;
