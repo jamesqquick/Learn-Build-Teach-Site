1d93ead79611e41a2a08e89522af0317ae18c74e
James Quick <james.q.quick@outlook.com>
2019-11-28 09:25:40 -0600

    Centering youtube button in h4

--- src/elements/CourseList.js
+++ src/elements/CourseList.js
@@ -1,14 +1,21 @@
-import styled from 'styled-components';
-import {primary} from "../utilities/Colors";
+import styled from "styled-components"
+import { primary } from "../utilities/Colors"
 
 export const StyledCourseList = styled.div`
-    h1 {
-        color: ${primary};
-        margin-bottom: 2rem;
-    }
+  h1 {
+    color: ${primary};
+    margin-bottom: 2rem;
+  }
 
-    .callout {
-        text-align: center;
-        font-weight: 300;
+  .callout {
+    text-align: center;
+    font-weight: 300;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    strong {
+      margin: 0px 0.5rem;
     }
-`;
+  }
+`
