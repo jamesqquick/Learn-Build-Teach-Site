84a70bd37da2cd35fd59aa0132f444fe9b7b9c97
James Quick <james.q.quick@outlook.com>
2019-11-28 09:39:15 -0600

    styling on youtube close btn

--- src/elements/FullScreenVideo.js
+++ src/elements/FullScreenVideo.js
@@ -1,46 +1,49 @@
-import styled from "styled-components";
-import {primaryDark} from "../utilities/Colors";
+import styled from "styled-components"
+import { primaryDark, white, lightGrey } from "../utilities/Colors"
 export const StyledFullScreenVideo = styled.div`
-    position: fixed;
-    z-index: 1000;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: ${primaryDark};
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${primaryDark};
+  padding: 2rem;
 
-    #closeIcon {
-        position: absolute;
-        top: 3rem;
-        right: 3rem;
-        color: white;
+  .video-responsive {
+    overflow: hidden;
+    position: relative;
+    width: 100%;
 
-        &:hover {
-            cursor: pointer;
-        }
-    }
-    
-    @media only screen and(max-width: 1150px) {
-        #videoEmbed {
-            width: 560px;
-            height: 315px;
-        }
+    &::after {
+      padding-top: 56.25%;
+      display: block;
+      content: "";
     }
 
-    @media only screen and(max-width: 650px) {
-        #videoEmbed {
-            width: 420px;
-            height: 236px;
-        }
+    iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
     }
+  }
 
-    @media only screen and(max-width: 400px) {
-        #videoEmbed {
-            width: 315px;
-            height: 177;
-        }
+  #closeIcon {
+    position: absolute;
+    top: 3rem;
+    right: 3rem;
+    color: ${lightGrey};
+    transition: transfom 500ms;
+
+    &:hover {
+      cursor: pointer;
+      color: white;
+      transform: scale(1.2);
     }
+  }
 `
